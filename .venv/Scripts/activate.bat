2375294ca5b03379a67b14bddd099debab617e5c
Soundheard <mxnmn.schndr@gmail.com>
2025-06-16 11:42:40 +0200

    ristky update WIP

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\mxnmn\PycharmProjects\secure_python\.venv
+set VIRTUAL_ENV=c:\Users\mxmns\Documents\code\secure_python_framework\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- test_file.py
+++ test_file.py
@@ -1,1 +1,1 @@
-print("hello world")
+print("hallo-welt")
